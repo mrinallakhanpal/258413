98c6dd3be23f8385a382d850ae61463e83a69ccc
Mrinal Lakhanpal <53489451+mrinallakhanpal@users.noreply.github.com>
2021-04-20 16:54:14 +0530

    Update DailyWorkingFile.py

--- DailyWorkingFile.py
+++ DailyWorkingFile.py
@@ -1,3 +1,13 @@
 print("Hello L&T Technology Services")
 print("hello world")
 def mr:
+  # This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
