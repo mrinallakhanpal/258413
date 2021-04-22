c6fd229390936831ea8d4844ff440387271892e1
Mrinal Lakhanpal <53489451+mrinallakhanpal@users.noreply.github.com>
2021-04-22 19:15:13 +0530

    Update DailyWorkingFile.py

--- DailyWorkingFile.py
+++ DailyWorkingFile.py
@@ -27,3 +27,18 @@ num = 8
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+
+
+a = 5
+b = 6
+c = 7
+
+
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
