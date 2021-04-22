9092a60a35a4ae8c803c51ce21e76da005c73b82
Mrinal Lakhanpal <53489451+mrinallakhanpal@users.noreply.github.com>
2021-04-22 19:13:28 +0530

    Update DailyWorkingFile.py

--- DailyWorkingFile.py
+++ DailyWorkingFile.py
@@ -9,10 +9,9 @@ num2 = 6.3
 # Add two numbers
 sum = num1 + num2
 
-# Display the sum
+
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
-# Store input numbers
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
@@ -21,3 +20,10 @@ sum = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+num = 8 
+
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
