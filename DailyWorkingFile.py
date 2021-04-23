f20832ef7a0c953be7799e78200cdd8740d2532d
Mrinal Lakhanpal <53489451+mrinallakhanpal@users.noreply.github.com>
2021-04-23 19:34:30 +0530

    Update DailyWorkingFile.py

--- DailyWorkingFile.py
+++ DailyWorkingFile.py
@@ -46,11 +46,7 @@ print('The area of the triangle is %0.2f' %area)
 x = 5
 y = 10
 
-# To take inputs from the user
-#x = input('Enter value of x: ')
-#y = input('Enter value of y: ')
 
-# create a temporary variable and swap the values
 temp = x
 x = y
 y = temp
