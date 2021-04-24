d7ec3b6e4e82ef63b7cd7180ae0a749e380fe118
Mrinal Lakhanpal <53489451+mrinallakhanpal@users.noreply.github.com>
2021-04-24 14:40:19 +0530

    Update DailyWorkingFile.py

--- DailyWorkingFile.py
+++ DailyWorkingFile.py
@@ -53,3 +53,10 @@ y = temp
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
+
+x = 5
+y = 10
+
+x, y = y, x
+print("x =", x)
+print("y =", y)
