fd9894e0e4f228ac17b4e4396b6d60d25968c51c
Mrinal Lakhanpal <53489451+mrinallakhanpal@users.noreply.github.com>
2021-04-28 19:24:14 +0530

    Update DailyWorkingFile.py

--- DailyWorkingFile.py
+++ DailyWorkingFile.py
@@ -23,9 +23,8 @@ sum = float(num1) + float(num2)
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 
+
 num = 8 
-
-
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
 
